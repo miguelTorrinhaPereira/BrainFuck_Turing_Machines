527e3b2094cfc8534fdb06031328d20f1016c200
Miguel Torrinha <miguel.torrinha.pereira+github@gmail.com>
2026-06-27 19:38:11 +0100

    Improved the simulation code

--- turing_machine_bf.c
+++ turing_machine_bf.c
@@ -1,12 +1,14 @@
 //TODO: OUTPUT
 // high level cell = [ Marker | Data ]
 #define CELL_SIZE 2
-// [copy cell][curr symbol][curr state](states: (state: [state_header](actions: (action: [new state][new sym][move]), ...)), ...)[tape barrier](tape: [tape cell], ...)
+// [copy cell][curr symbol][curr state](states: (state: [state_header](actions: (action: [new state][new sym][move][empty cell]), ...)), ...)[tape barrier](tape: [tape cell], ...)
 
-// final states, stored in the movement cell
-#define ABORT_VALUE 1
+// movement cell values, also includes final states
+#define LEFT_VALUE 0
+#define RIGHT_VALUE 1
 #define ACCEPT_VALUE 2
 #define REJECT_VALUE 3
+#define ABORT_VALUE 4
 
 // used to avoid spaces
 #define CODE(code) code
@@ -62,9 +64,8 @@
 #define FALSE 0
 #define TRUE 1
 
-// set mark
+// mark cell
 #define MARK_CELL(value) SHIFT_TO_MARKER()SET_CELL(value)RET_MARKER()
-#define UNMARK_CELL() MARK_CELL(UNMARKED)
 
 // conditional
 // sets the cell 2 cells to the right to 0
@@ -159,37 +160,42 @@
 	/* leave it at 0, this will be used as tape */ \
 )
 
+
+#define FIND_ACTION() CODE( \
+	/* setup array search to get the curr state */ \
+	/* the previous ARRAY_HEAD was the movement cell of some action */ \
+	/* this is usefull for the terminal states, they can put 2 and 3 for aceptance and rejection states */ \
+	GOTO_MARKER(ARRAY_HEAD) \
+	MARK_CELL(UNMARKED) \
+	/* go to the first cell of the first state */ \
+	GOTO_MARKER(STATE_START) \
+	MARK_CELL(ARRAY_HEAD) \
+	/* go back to the curr state cell */ \
+	GOTO_MARKER(CURR_STATE_CELL) \
+	/* the index cell end with the value 0 */ \
+	ARRAY_SEARCH(CURR_STATE_CELL, STATE_START) \
+	\
+	/* copy the curr symbol of the turing machine */ \
+	GOTO_MARKER(TAPE_HEAD) \
+	COPY(TAPE_HEAD, CURR_SYM_CELL) \
+	GOTO_MARKER(CURR_SYM_CELL) \
+	\
+	/* get the correct action, we are already have marked the correct state */ \
+	GOTO_MARKER(ARRAY_HEAD) \
+	/* skip state header */ \
+	MARK_CELL(STATE_START) \
+	RIGHT(1) \
+	MARK_CELL(ARRAY_HEAD) \
+	GOTO_MARKER(CURR_SYM_CELL) \
+	ARRAY_SEARCH(CURR_SYM_CELL, ACTION_START) \
+)
+
 #define SIMULATE() CODE( \
 	/* expected starting point for the loop, continues until the curr state is 0 */ \
 	GOTO_MARKER(CURR_STATE_CELL) \
 	/* main loop */ \
 	WHILE_NZ( \
-		/* setup array search to get the curr state */ \
-		/* the previous ARRAY_HEAD was the movement cell of some action */ \
-		/* this is usefull for the terminal states, they can put 2 and 3 for aceptance and rejection states */ \
-		GOTO_MARKER(ARRAY_HEAD) \
-		UNMARK_CELL() \
-		/* go to the first cell of the first state */ \
-		GOTO_MARKER(STATE_START) \
-		MARK_CELL(ARRAY_HEAD) \
-		/* go back to the curr state cell */ \
-		GOTO_MARKER(CURR_STATE_CELL) \
-		/* the index cell end with the value 0 */ \
-		ARRAY_SEARCH(CURR_STATE_CELL, STATE_START) \
-		\
-		/* copy the curr symbol of the turing machine */ \
-		GOTO_MARKER(TAPE_HEAD) \
-		COPY(TAPE_HEAD, CURR_SYM_CELL) \
-		GOTO_MARKER(CURR_SYM_CELL) \
-		\
-		/* get the correct action, we are already have marked the correct state */ \
-		GOTO_MARKER(ARRAY_HEAD) \
-		/* skip state header */ \
-		MARK_CELL(STATE_START) \
-		RIGHT(1) \
-		MARK_CELL(ARRAY_HEAD) \
-		GOTO_MARKER(CURR_SYM_CELL) \
-		ARRAY_SEARCH(CURR_SYM_CELL, ACTION_START) \
+		FIND_ACTION()  /* marks with ARRAY_HEAD the starting cell of the correct action */ \
 		\
 		/* copy the next state to curr state cell */ \
 		GOTO_MARKER(ARRAY_HEAD) \
@@ -204,43 +210,42 @@
 		COPY(ARRAY_HEAD, TAPE_HEAD) \
 		\
 		/* go to the move cell of the action */ \
-		UNMARK_CELL() \
+		MARK_CELL(UNMARKED) \
 		RIGHT(1) \
 		MARK_CELL(ARRAY_HEAD) \
 		\
-		/* move is 1 for right, 0 for left */ \
-		WHILE_NZ( \
-			/* we need to move it to the right, so move the head to the right 2 times */ \
-			/* latter it will be moved it to the left one time */ \
+		IF_EQUAL(LEFT_VALUE,  /* RIGHT */ \
 			GOTO_MARKER(TAPE_HEAD) \
-			UNMARK_CELL() \
-			RIGHT(2) \
+			MARK_CELL(UNMARKED) \
+			RIGHT(1) \
 			MARK_CELL(TAPE_HEAD) \
 			GOTO_MARKER(ARRAY_HEAD) \
 		) \
-		GOTO_MARKER(TAPE_HEAD) \
-		\
-		/* test to see if it is the tape barrier */ \
-		LEFT(1) \
-		SHIFT_TO_MARKER() \
-			IF_EQUAL(TAPE_BARRIER, \
-				/* this is the tape barrier! abort! */ \
-				RET_MARKER() \
-				/*  put the curr state to 0 so it stops */ \
-				GOTO_MARKER(CURR_STATE_CELL) \
-				SET_CELL(0) \
-				/* put the movement to ABORT (1) to know the cause of the stop */ \
-				GOTO_MARKER(ARRAY_HEAD) \
-				SET_CELL(ABORT_VALUE) \
-				\
-				GOTO_MARKER(TAPE_BARRIER) \
-				SHIFT_TO_MARKER() \
-			) \
-		RET_MARKER() \
-		\
-		/* actually do the left movement */ \
-		/* The IF_EQUAL already set the old head to 0 */ \
-		MARK_CELL(TAPE_HEAD) \
+		IF_EQUAL(RIGHT_VALUE,  /* LFET */ \
+			GOTO_MARKER(TAPE_HEAD) \
+			\
+			/* test to see if it is the tape barrier */ \
+			LEFT(1) \
+			SHIFT_TO_MARKER() \
+				IF_EQUAL(TAPE_BARRIER, \
+					/* this is the tape barrier! abort! */ \
+					RET_MARKER() \
+					/*  put the curr state to 0 so it stops */ \
+					GOTO_MARKER(CURR_STATE_CELL) \
+					SET_CELL(0) \
+					/* put the movement to ABORT (1) to know the cause of the stop */ \
+					GOTO_MARKER(ARRAY_HEAD) \
+					SET_CELL(ABORT_VALUE) \
+					\
+					GOTO_MARKER(TAPE_BARRIER) \
+					SHIFT_TO_MARKER() \
+				) \
+			RET_MARKER() \
+			\
+			/* actually do the left movement */ \
+			/* The IF_EQUAL already set the old head to 0 */ \
+			MARK_CELL(TAPE_HEAD) \
+		) \
 		\
 		/* if the state is 0, the machine stops */ \
 		GOTO_MARKER(CURR_STATE_CELL) \
@@ -259,24 +264,25 @@
 	   INC() \
 )
 
+//TODO: remove the if else logic, unecessary
 #define OUTPUT_RESULT() CODE(\
 	GOTO_MARKER(CURR_STATE_CELL)  /* wouldn't be used anymore */ \
 	SET_CELL(TRUE) \
 	\
 	/* If, else if, else if statment */ \
-	WHILE_NZ(  /* 1 for abort */ \
+	WHILE_NZ( \
 		GOTO_MARKER(ARRAY_HEAD) \
 		IF_EQUAL(ABORT_VALUE, DO_ABORT()) \
 		GOTO_MARKER(CURR_STATE_CELL) \
 		SET_CELL(FALSE) \
 	) \
-	WHILE_NZ(  /* 2 for accept */ \
+	WHILE_NZ( \
 		GOTO_MARKER(ARRAY_HEAD) \
 		IF_EQUAL(ACCEPT_VALUE, DO_ACCEPT()) \
 		GOTO_MARKER(CURR_STATE_CELL) \
 		SET_CELL(FALSE) \
 	) \
-	WHILE_NZ(  /* 3 for reject */ \
+	WHILE_NZ( \
 		GOTO_MARKER(ARRAY_HEAD) \
 		IF_EQUAL(REJECT_VALUE, DO_REJECT()) \
 	) \
